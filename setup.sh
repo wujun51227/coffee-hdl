e6ccbf7d2f715f7a77640d6c9b5246883befbc74
thriller_wu <woodford_wu@163.com>
2019-09-23 21:55:43 +0800

    add setup.sh for library install

---
+++ setup.sh
@@ -0,0 +1,5 @@
+chdl_lib.coffee ./lib/chdl_component_lib.chdl
+chdl_lib.coffee ./lib/chdl_primitive_lib.chdl
+echo "============================"
+echo "Library install finished"
+echo "============================"
